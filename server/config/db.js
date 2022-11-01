b2dd79e88b69026c47494341a1aea2dd6a61d27f
aobcena <aobcena@my.centennicalcollege.ca>
2022-11-01 03:03:04 -0400

    dbase uri update

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
- URI: "mongodb://127.0.0.1:27017/contactlist",
- // URI: "mongodb+srv://albert:albert123@cluster0.ex5qg7g.mongodb.net/book_store?retryWrites=true&w=majority"
+ //URI: "mongodb://127.0.0.1:27017/contactlist",
+  URI: "mongodb+srv://albert:albert123@cluster0.ex5qg7g.mongodb.net/book_store?retryWrites=true&w=majority"
   
 };
